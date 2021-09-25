8c0410994b5e945c5a4e2f61db113bba8ed9614f
Roman Mendaliev <reacheight8@gmail.com>
2021-09-25 14:45:23 +0300

    Insert Entities processor

--- src/main/scala/CourierProcessor.scala
+++ src/main/scala/CourierProcessor.scala
@@ -1,18 +1,21 @@
 package wind
 
+import skadistats.clarity.event.Insert
 import skadistats.clarity.model.{Entity, FieldPath}
 import skadistats.clarity.processor.entities.{Entities, OnEntityPropertyChanged}
-import skadistats.clarity.processor.runner.Context
 
 class CourierProcessor {
   var courierOutOfFountain: Map[Int, Boolean] = Map()
 
+  @Insert
+  private val entities: Entities = null
+
   @OnEntityPropertyChanged(classPattern = "CDOTAGamerulesProxy.*", propertyPattern = "m_pGameRules.m_flGameStartTime")
-  def onGameStartTimeChanged(ctx: Context, e: Entity, fp: FieldPath[_ <: FieldPath[_ <: AnyRef]]): Unit = {
+  def onGameStartTimeChanged(e: Entity, fp: FieldPath[_ <: FieldPath[_ <: AnyRef]]): Unit = {
     val gameTimeState = Util.getGameTimeState(e)
 
     if (gameTimeState.gameStarted) {
-      val courierEntities = ctx.getProcessor(classOf[Entities]).getAllByPredicate(e => e.getDtClass.getDtName.startsWith("CDOTA_Unit_Courier"))
+      val courierEntities = entities.getAllByPredicate(e => e.getDtClass.getDtName.startsWith("CDOTA_Unit_Courier"))
       courierEntities.forEachRemaining(courier => {
         val playerId = courier.getProperty[Int]("m_nPlayerOwnerID")
         val (x, y) = Util.getLocation(courier)

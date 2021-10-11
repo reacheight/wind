1df8673a43e155d863c29ab1fd58f11ed4e92bc3
Roman Mendaliev <reacheight8@gmail.com>
2021-10-11 22:22:10 +0300

    Add more data points to calculate hero lane

--- src/main/scala/processors/LaneProcessor.scala
+++ src/main/scala/processors/LaneProcessor.scala
@@ -11,8 +11,10 @@ import scala.collection.mutable.ArrayBuffer
 
 class LaneProcessor {
   private val EPS = 0.001f
+  private val ITERATION_INTERVAL = 10
+  private var currentIteration = 1
   private val LANE_STAGE_END_MINUTE = 10
-  private var currentMin = 1
+  private val LANE_STAGE_ITERATION_COUNT = 60 * LANE_STAGE_END_MINUTE / ITERATION_INTERVAL
   private var heroLocationMap = Map[Int, ArrayBuffer[(Float, Float)]]()
 
   var heroLaneStageLocation: Map[Int, (Float, Float)] = Map()
@@ -23,7 +25,7 @@ class LaneProcessor {
   @OnEntityPropertyChanged(classPattern = "CDOTAGamerulesProxy.*", propertyPattern = "m_pGameRules.m_fGameTime")
   def onGameTimeChanged(ctx: Context, gameRulesEntity: Entity, fp: FieldPath[_ <: FieldPath[_ <: AnyRef]]): Unit = {
     val gameTimeState = Util.getGameTimeState(gameRulesEntity)
-    if (!gameTimeState.gameStarted || currentMin > LANE_STAGE_END_MINUTE || currentMin * 60 - gameTimeState.gameTime > EPS) return
+    if (!gameTimeState.gameStarted || currentIteration > LANE_STAGE_ITERATION_COUNT || currentIteration * ITERATION_INTERVAL - gameTimeState.gameTime > EPS) return
 
     val entities = ctx.getProcessor(classOf[Entities])
     val heroEntities = entities.getAllByPredicate(Util.isHero)
@@ -37,7 +39,7 @@ class LaneProcessor {
         heroLocationMap += (playerId -> ArrayBuffer(location))
     })
 
-    if (currentMin == LANE_STAGE_END_MINUTE) {
+    if (currentIteration == LANE_STAGE_END_MINUTE) {
       heroLaneStageLocation = heroLocationMap.map(item => {
         val (playerId, locations) = item
         val xs = locations.map(_._1)
@@ -62,7 +64,7 @@ class LaneProcessor {
       })
     }
 
-    currentMin += 1
+    currentIteration += 1
   }
 
   private def getPlayersExpAndNetworth(data: Entity): Map[Int, (Int, Int)] = {

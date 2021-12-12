e271ea677366d6cb870fe72d417c5d9e6decb65d
Roman Mendaliev <reacheight8@gmail.com>
2021-12-12 14:33:07 +0300

    Refactor glyph processor

--- src/main/scala/wind/processors/GlyphProcessor.scala
+++ src/main/scala/wind/processors/GlyphProcessor.scala
@@ -6,19 +6,22 @@ import skadistats.clarity.processor.runner.Context
 import wind.Team._
 import wind.Util
 
+import scala.collection.{immutable, mutable}
+
 class GlyphProcessor {
-  var glyphNotUsedOnT1: Map[Team, Int] = Map()
+  def glyphNotUsedOnT1: immutable.Map[Team, Int] = _glyphNotUsedOnT1.toMap
+
+  private val _glyphNotUsedOnT1 = mutable.Map[Team, Int]()
 
   @OnEntityPropertyChanged(classPattern = "CDOTA_BaseNPC_Tower", propertyPattern = "m_lifeState")
   def onTowerLifeStateChanged(ctx: Context, tower: Entity, fp: FieldPath[_ <: FieldPath[_ <: AnyRef]]): Unit = {
-    if (tower.getProperty[Int]("m_iCurrentLevel") != 1) return
-    if (tower.getPropertyForFieldPath[Int](fp) != 1) return
-
-    val towerTeam = if (tower.getProperty[Int]("m_iTeamNum") == 2) Radiant else Dire
-    val gameRules = ctx.getProcessor(classOf[Entities]).getByDtName("CDOTAGamerulesProxy")
-    if (!Util.isGlyphOnCooldown(gameRules, towerTeam)) {
-      val current = glyphNotUsedOnT1.getOrElse(towerTeam, 0)
-      glyphNotUsedOnT1 += towerTeam -> (current + 1)
+    if (tower.getProperty[Int]("m_iCurrentLevel") == 1 && tower.getPropertyForFieldPath[Int](fp) == 1) {
+      val towerTeam = if (tower.getProperty[Int]("m_iTeamNum") == 2) Radiant else Dire
+      val gameRules = ctx.getProcessor(classOf[Entities]).getByDtName("CDOTAGamerulesProxy")
+      if (!Util.isGlyphOnCooldown(gameRules, towerTeam)) {
+        val current = _glyphNotUsedOnT1.getOrElse(towerTeam, 0)
+        _glyphNotUsedOnT1 += towerTeam -> (current + 1)
+      }
     }
   }
 }

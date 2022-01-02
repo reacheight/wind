be6bf07cc7a07aefb705e19ee5ad695d9f206457
Roman Mendaliev <reacheight8@gmail.com>
2022-01-02 17:56:06 +0300

    Shitty way to get public matches

--- src/main/scala/wind/OdotaClient.scala
+++ src/main/scala/wind/OdotaClient.scala
@@ -1,14 +1,19 @@
 package wind
 
 import io.circe.Decoder
+import io.circe.generic.semiauto.deriveDecoder
 import sttp.client3.circe._
 import sttp.client3.quick._
 
 case class ReplayLocation(matchId: Long, cluster: Long, salt: Long)
+// todo make serialization not that shitty
+case class Match(match_id: Long, match_seq_num: Long, radiant_win: Boolean, start_time: Long, duration: Long, avg_mmr: Option[Int], num_mmr: Option[Int], lobby_type: Int, game_mode: Int, avg_rank_tier: Int, num_rank_tier: Int, cluster: Int, radiant_team: String, dire_team: String)
 
 object OdotaClient {
   private implicit val decodeReplayLocation: Decoder[ReplayLocation] =
     Decoder.forProduct3("match_id", "cluster", "replay_salt")(ReplayLocation.apply)
+
+  private implicit val decodeMatch: Decoder[Match] = deriveDecoder
 
   def getReplayLocation(matchId: String): Option[ReplayLocation] = {
     val response = quickRequest
@@ -17,5 +22,14 @@ object OdotaClient {
       .send(backend)
 
     response.body.toOption.flatMap(list => list.headOption)
+  }
+
+  def getPublicMatches: Option[List[Match]] = {
+    val response = quickRequest
+      .get(uri"https://api.opendota.com/api/publicMatches?mmr_descending=1")
+      .response(asJson[List[Match]])
+      .send(backend)
+
+    response.body.toOption
   }
 }

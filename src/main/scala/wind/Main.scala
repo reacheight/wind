caa1f33bd32f123869114e638c9ae5ec3110e934
Roman Mendaliev <reacheight8@gmail.com>
2022-01-02 20:08:20 +0300

    Arg to run win prob data collecting

--- src/main/scala/wind/Main.scala
+++ src/main/scala/wind/Main.scala
@@ -17,24 +17,36 @@ object Main {
         replayLocation match {
           case None => println(s"Can't find replay for match ${`match`}.")
           case Some(location) =>
-            println("Downloading replay...")
-            ReplayDownloader.downloadReplay(location, compressedReplayPath)
-
-            println("Decompressing replay..")
-            BZip2Decompressor.decompress(compressedReplayPath, replayPath)
-
+            downloadReplay(location)
             println("Analyzing replay...")
             analyze(replayPath)
-
-            compressedReplayPath.toFile.delete()
-            replayPath.toFile.delete()
-            downloadingDirectory.toFile.delete()
+            deleteReplayFolder()
         }
       }
       else {
         println("Analyzing replay...")
         analyze(Paths.get(`match`))
       }
+    }
+    else if (args(0) == "collect") {
+      collect()
+    }
+  }
+
+  def collect(): Unit = {
+    val collector = WinProbabilityDataCollector
+    val matches = OdotaClient.getPublicMatches
+    matches match {
+      case Some(matches) => matches.filter(m => m.avg_mmr.getOrElse(0) > 3000).foreach(m => {
+        val replayLocation = OdotaClient.getReplayLocation(m.match_id.toString)
+        replayLocation match {
+          case Some(location) =>
+            downloadReplay(location)
+            println("Collecting data..")
+            collector.collect(replayPath, m.match_id.toString)
+            deleteReplayFolder()
+        }
+      })
     }
   }
 
@@ -92,5 +104,19 @@ object Main {
     result.obsPlacedOnVision.foreach { case (id, time) =>
       println(s"${result.heroName(id)} ${time.toString}")
     }
+  }
+
+  def downloadReplay(location: ReplayLocation): Unit = {
+    println(s"Downloading replay..")
+    ReplayDownloader.downloadReplay(location, compressedReplayPath)
+
+    println("Decompressing replay..")
+    BZip2Decompressor.decompress(compressedReplayPath, replayPath)
+  }
+
+  def deleteReplayFolder(): Unit = {
+    compressedReplayPath.toFile.delete()
+    replayPath.toFile.delete()
+    downloadingDirectory.toFile.delete()
   }
 }

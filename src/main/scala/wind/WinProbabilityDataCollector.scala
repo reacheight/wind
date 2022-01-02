3cce467502072445c327aaf3fa390c7869bfbfb8
Roman Mendaliev <reacheight8@gmail.com>
2022-01-02 20:05:47 +0300

    Use hero processor in win probability collector

--- src/main/scala/wind/WinProbabilityDataCollector.scala
+++ src/main/scala/wind/WinProbabilityDataCollector.scala
@@ -3,7 +3,7 @@ package wind
 import skadistats.clarity.Clarity
 import skadistats.clarity.processor.runner.SimpleRunner
 import skadistats.clarity.source.MappedFileSource
-import wind.processors.WinProbabilityProcessor
+import wind.processors.{HeroProcessor, WinProbabilityProcessor}
 
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Path, Paths}
@@ -13,15 +13,16 @@ object WinProbabilityDataCollector {
   private val directory = Paths.get("win_prob_data")
 
   def collect(replay: Path, matchId: String): Unit = {
+    val gameInfo = Clarity.infoForFile(replay.toAbsolutePath.toString)
     val winProbabilityProcessor = new WinProbabilityProcessor
+    val heroProcessor = new HeroProcessor(gameInfo)
 
     Using.Manager { use =>
       val source = use(new MappedFileSource(replay))(s => s.close())
       val runner = new SimpleRunner(source)
-      runner.runWith(winProbabilityProcessor)
+      runner.runWith(winProbabilityProcessor, heroProcessor)
     }
 
-    val gameInfo = Clarity.infoForFile(replay.toAbsolutePath.toString)
     val winner = gameInfo.getGameInfo.getDota.getGameWinner - 2
     val content = winProbabilityProcessor.data.map(_.toString + s" $winner").mkString("\n")
     Files.write(Paths.get(directory.toString, s"$matchId.txt"), content.getBytes(StandardCharsets.UTF_8))

4dbae0d335bff521e7fb5db9b6f63dbc2d8b4b1c
Roman Mendaliev <reacheight8@gmail.com>
2022-01-07 15:33:14 +0300

    Format hero names

--- ui/src/Analysis/Analysis.js
+++ ui/src/Analysis/Analysis.js
@@ -1,25 +1,25 @@
 import React from 'react';
+import { formatHeroName } from '../util';
 
 import styles from './Analysis.module.css'
 
 export default class Analysis extends React.Component {
   render() {
     const analysis = this.props.analysis
-    const analysisLoaded = analysis && Object.keys(analysis).length !== 0
-    let courierInfo
-    if (analysisLoaded) {
-      courierInfo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((id) =>
-        <li key={id.toString}>
-          {analysis.heroes[id]}'s courier is {analysis.couriers[id] ? <span className={styles.green}>out of</span> : <span className={styles.red}>in</span>} fountain
-        </li>
-      )
+
+    if (!analysis || Object.keys(analysis).length === 0) {
+      return <div></div>
     }
+
+    const courierInfo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((id) =>
+      <li key={id.toString}>
+        {formatHeroName(analysis.heroes[id])}'s courier is {analysis.couriers[id] ? <span className={styles.green}>out of</span> : <span className={styles.red}>in</span>} fountain
+      </li>
+    )
     
     return (
       <div className={styles.analysis}>
-        {analysisLoaded &&
-          <ul>{courierInfo}</ul>
-        }
+        <ul>{courierInfo}</ul>
       </div>
     )
   }

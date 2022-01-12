2af6e9368a310a978eedf8c5309c660bb9bda81c
Roman Mendaliev <reacheight8@gmail.com>
2022-01-12 16:03:59 +0300

    Max stocks to functional components

--- ui/src/ObserversMaxStock/ObserversMaxStock.js
+++ ui/src/ObserversMaxStock/ObserversMaxStock.js
@@ -2,20 +2,17 @@ import React from "react";
 import styles from '../Analysis/Analysis.module.css'
 import '../items.css'
 
-export default class ObserversMaxStock extends React.Component {
-  render() {
-    return (
-      <>
-        <h5 className={styles.analysisTitle}><span className={"observer"}>Observers</span> max stock duration</h5>
-        <ul>
-          <li key={"observersMaxStockRadiant"}>
-            <span className={styles.green}>Radiant</span> — {this.props.observersMaxStock.Radiant} sec
-          </li>
-          <li key={"observersMaxStockDire"}>
-            <span className={styles.red}>Dire</span> — {this.props.observersMaxStock.Dire} sec
-          </li>
-        </ul>
-      </>
-    )
-  }
-}
+const ObserversMaxStock = (props) =>
+  <>
+    <h5 className={styles.analysisTitle}><span className={"observer"}>Observers</span> max stock duration</h5>
+    <ul>
+      <li key={"observersMaxStockRadiant"}>
+        <span className={styles.green}>Radiant</span> — {props.observersMaxStock.Radiant} sec
+      </li>
+      <li key={"observersMaxStockDire"}>
+        <span className={styles.red}>Dire</span> — {props.observersMaxStock.Dire} sec
+      </li>
+    </ul>
+  </>
+
+export default ObserversMaxStock
--- ui/src/SmokesMaxStock/SmokesMaxStock.js
+++ ui/src/SmokesMaxStock/SmokesMaxStock.js
@@ -2,20 +2,17 @@ import React from "react";
 import styles from '../Analysis/Analysis.module.css'
 import '../items.css'
 
-export default class SmokesMaxStock extends React.Component {
-  render() {
-    return (
-      <>
-        <h5 className={styles.analysisTitle}><span className="smoke">Smokes</span> max stock duration</h5>
-        <ul>
-          <li key={"smokesMaxStockRadiant"}>
-            <span className={styles.green}>Radiant</span> — {this.props.smokesMaxStock.Radiant} sec
-          </li>
-          <li key={"smokesMaxStockDire"}>
-            <span className={styles.red}>Dire</span> — {this.props.smokesMaxStock.Dire} sec
-          </li>
-        </ul>
-      </>
-    )
-  }
-}
+const SmokesMaxStock = (props) =>
+  <>
+    <h5 className={styles.analysisTitle}><span className="smoke">Smokes</span> max stock duration</h5>
+    <ul>
+      <li key={"smokesMaxStockRadiant"}>
+        <span className={styles.green}>Radiant</span> — {props.smokesMaxStock.Radiant} sec
+      </li>
+      <li key={"smokesMaxStockDire"}>
+        <span className={styles.red}>Dire</span> — {props.smokesMaxStock.Dire} sec
+      </li>
+    </ul>
+  </>
+
+export default SmokesMaxStock

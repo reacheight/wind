7da3b25329006ead781691509e7f90929d7e2aa6
Roman Mendaliev <reacheight8@gmail.com>
2022-01-12 15:33:37 +0300

    Obs and smokes to functional components and cleanup

--- ui/src/Observers/Observers.js
+++ ui/src/Observers/Observers.js
@@ -1,29 +1,21 @@
 import React from "react";
 import styles from '../Analysis/Analysis.module.css'
 import '../items.css'
-import { compareTime } from "../util";
 
-export default class Observers extends React.Component {
-  render() {
-    const obsInfo = Object.keys(this.props.observers)
-      .map((id) => this.props.observers[id].map((time) => [id, time]))
-      .flat()
-      .sort((first, second) => compareTime(first[1], second[1]))
-      .map((pair) =>
-        <li key={pair[1]}>
-          <span className={styles.heroName}>{this.props.heroes[pair[0]]}</span> at {pair[1]}
-        </li>
-      )
-    
-    if (obsInfo.length === 0) {
-      return <></>
-    }
-    
-    return (
-      <>
-        <h5 className={styles.analysisTitle}><span className="observer">Observers</span> placed on enemy vision 👀</h5>
-        <ul>{obsInfo}</ul>
-      </>
+const Observers = (props) => {
+  const obsInfo = props.observers
+    .map((entry) =>
+      <li key={"observer" + entry[0]}>
+        <span className={styles.heroName}>{props.heroes[entry[1]]}</span> at {entry[0]}
+      </li>
     )
-  }
+    
+  return (
+    <>
+      <h5 className={styles.analysisTitle}><span className="observer">Observers</span> placed on enemy vision 👀</h5>
+      <ul>{obsInfo}</ul>
+    </>
+  )
 }
+
+export default Observers
--- ui/src/Smokes/Smokes.js
+++ ui/src/Smokes/Smokes.js
@@ -1,29 +1,21 @@
 import React from "react"
 import styles from '../Analysis/Analysis.module.css'
 import '../items.css'
-import { compareTime } from "../util"
 
-export default class Smokes extends React.Component {
-  render() {
-    const smokeInfo = Object.keys(this.props.smokes)
-      .map((id) => this.props.smokes[id].map((time) => [id, time]))
-      .flat()
-      .sort((first, second) => compareTime(first[1], second[1]))
-      .map((pair) =>
-        <li key={pair[1]}>
-          <span className={styles.heroName}>{this.props.heroes[pair[0]]}</span> at {pair[1]}
-        </li>
-      )
-    
-    if (smokeInfo.length === 0) {
-      return <></>
-    }
-    
-    return (
-      <>
-        <h5 className={styles.analysisTitle}><span className="smoke">Smokes</span> used on enemy vision 💨</h5>
-        <ul>{smokeInfo}</ul>
-      </>
+const Smokes = (props) => {
+  const smokeInfo = props.smokes
+    .map((entry) =>
+      <li key={"smoke" + entry[0]}>
+        <span className={styles.heroName}>{props.heroes[entry[1]]}</span> at {entry[0]}
+      </li>
     )
-  }
+
+  return (
+    <>
+      <h5 className={styles.analysisTitle}><span className="smoke">Smokes</span> used on enemy vision 💨</h5>
+      <ul>{smokeInfo}</ul>
+    </>
+  )
 }
+
+export default Smokes

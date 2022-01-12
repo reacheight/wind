b43a79a0d4c92d0eb6ff9f2bc665b3b55e61d112
Roman Mendaliev <reacheight8@gmail.com>
2022-01-12 15:23:10 +0300

    Couriers and header to functional component

--- ui/src/Couriers/Couriers.js
+++ ui/src/Couriers/Couriers.js
@@ -1,19 +1,19 @@
 import React from "react";
 import styles from '../Analysis/Analysis.module.css'
 
-export default class Couriers extends React.Component {
-  render() {
-    const courierInfo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((id) =>
-      <li key={id.toString()}>
-        <span className={styles.heroName}>{this.props.heroes[id]}</span>'s courier is {this.props.couriers[id] ? <span className={styles.green}>out of</span> : <span className={styles.red}>in</span>} fountain
-      </li>
-    )
+const Couriers = (props) => {
+  const courierInfo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((id) =>
+    <li key={id.toString()}>
+      <span className={styles.heroName}>{props.heroes[id]}</span>'s courier is {props.couriers[id] ? <span className={styles.green}>out of</span> : <span className={styles.red}>in</span>} fountain
+    </li>
+  )
 
-    return (
-      <>
-        <h5 className={styles.analysisTitle}>Couriers 🐔</h5>
-        <ul>{courierInfo}</ul>
-      </>
-    )
-  }
+  return (
+    <>
+      <h5 className={styles.analysisTitle}>Couriers 🐔</h5>
+      <ul>{courierInfo}</ul>
+    </>
+  )
 }
+
+export default Couriers

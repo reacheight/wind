3d2e6a2a73f39175bb0cd495477530152ccff5cf
Roman Mendaliev <reacheight8@gmail.com>
2022-02-18 20:41:01 +0300

    Simplify json encoding

--- src/main/scala/wind/WindApp.scala
+++ src/main/scala/wind/WindApp.scala
@@ -2,6 +2,7 @@ package wind
 
 import cats.effect._
 import io.circe.{Encoder, Json}
+import io.circe.syntax._
 import org.http4s.HttpRoutes
 import org.http4s.blaze.server._
 import org.http4s.circe.CirceEntityCodec.circeEntityEncoder
@@ -10,76 +11,52 @@ import org.http4s.implicits._
 import org.http4s.server.middleware._
 import wind.models.Lane.Lane
 import wind.models.Team.Team
-import wind.models.{GameTimeState, PlayerId}
+import wind.models.{Fight, GameTimeState, PlayerId}
 
 import java.nio.file.{Files, Paths}
 import scala.concurrent.ExecutionContext.global
 
 object WindApp extends IOApp {
-  implicit def fromStringSeq: Encoder[Seq[String]] = (a: Seq[String]) =>
-    Json.fromValues(a.map(Json.fromString))
+  def fromPlayerIdKeyMap[T](implicit valueEncoder: Encoder[T]): Encoder[Map[PlayerId, T]] = (map: Map[PlayerId, T]) =>
+    map.map { case (id, value) => id.id -> value }.asJson
 
-  implicit def stringMapEncoder: Encoder[Map[PlayerId, String]] = (a: Map[PlayerId, String]) =>
-    Json.fromFields(a.map { case (id, value) => id.toString -> Json.fromString(value) })
+  def fromTeamKeyMap[T](implicit valueEncoder: Encoder[T]): Encoder[Map[Team, T]] = (map: Map[Team, T]) =>
+    map.map { case (team, value) => team.id -> value }.asJson
 
-  implicit def boolMapEncoder: Encoder[Map[PlayerId, Boolean]] = (a: Map[PlayerId, Boolean]) =>
-    Json.fromFields(a.map { case (id, value) => id.toString -> Json.fromBoolean(value) })
+  implicit val fromPlayerId: Encoder[PlayerId] = (id: PlayerId) => id.id.asJson
+  implicit val fromTeam: Encoder[Team] = (team: Team) => team.id.asJson
+  implicit val fromLane: Encoder[Lane] = (lane: Lane) => lane.id.asJson
 
-  implicit def intMapEncoder: Encoder[Map[PlayerId, Int]] = (a: Map[PlayerId, Int]) =>
-    Json.fromFields(a.map { case (id, value) => id.toString -> Json.fromInt(value)})
+  implicit val fromGameTimeState: Encoder[GameTimeState] = (time: GameTimeState) => time.toString.asJson
+  implicit val fromFight: Encoder[Fight] = (fight: Fight) => (fight.outnumberedTeam.get, fight.start).asJson
 
-  implicit def floatMapEncoder: Encoder[Map[PlayerId, Float]] = (a: Map[PlayerId, Float]) =>
-    Json.fromFields(a.map { case (id, value) => id.toString -> Json.fromFloatOrNull(value)})
-
-  implicit def intTupleEncoder: Encoder[(Int, Int)] = (a: (Int, Int)) =>
-    Json.fromValues(Seq(Json.fromInt(a._1), Json.fromInt(a._2)))
-
-  implicit def tupleIntMapEncoder: Encoder[Map[PlayerId, (Int, Int)]] = (a: Map[PlayerId, (Int, Int)]) =>
-    Json.fromFields(a.map { case (id, tuple) => id.toString -> intTupleEncoder(tuple) })
-
-  implicit def timeToPlayerId: Encoder[Seq[(GameTimeState, PlayerId)]] = (a: Seq[(GameTimeState, PlayerId)]) =>
-    Json.fromFields(a.map { case (time, id) => time.toString -> Json.fromString(id.toString) })
-
-  implicit def teamToTime: Encoder[Map[Team, GameTimeState]] = (a: Map[Team, GameTimeState]) =>
-    Json.fromFields(a.map { case (team, time) => team.id.toString -> Json.fromString(time.toString) })
-
-  implicit def teamToInt: Encoder[Map[Team, Int]] = (a: Map[Team, Int]) =>
-    Json.fromFields(a.map { case (team, value) => team.id.toString -> Json.fromInt(value) })
-
-  implicit def timeIdStringSeq: Encoder[Seq[(GameTimeState, PlayerId, String)]] = (a: Seq[(GameTimeState, PlayerId, String)]) =>
-    Json.fromValues(a.map { case (time, id, value) => Json.fromValues(Seq(Json.fromString(time.toString), Json.fromString(id.toString), Json.fromString(value))) })
-
-  implicit def timeIdIdStringSeq: Encoder[Seq[(GameTimeState, PlayerId, PlayerId, String)]] = (a: Seq[(GameTimeState, PlayerId, PlayerId, String)]) =>
-    Json.fromValues(a.map { case (time, id1, id2, value) => Json.fromValues(Seq(Json.fromString(time.toString), Json.fromString(id1.toString), Json.fromString(id2.toString), Json.fromString(value))) })
-
-  implicit def fromPurchases: Encoder[Map[String, Seq[(String, Int)]]] = (a: Map[String, Seq[(String, Int)]]) =>
-    Json.fromFields(a.map { case (hero, list) => hero -> Json.fromValues(list.map { case (item, time) => Json.fromValues(Seq(Json.fromString(item), Json.fromInt(time))) })})
-
-  implicit def fromWastedCreepwaves: Encoder[Seq[(GameTimeState, Team, Lane, Int)]] = (a: Seq[(GameTimeState, Team, Lane, Int)]) =>
-    Json.fromValues(a.map { case (time, team, lane, tier) => fromStringSeq(Seq(time.toString, team.id.toString, lane.id.toString, tier.toString)) })
-
-  implicit def fromTimePlayerIdSeq: Encoder[Seq[(GameTimeState, PlayerId)]] = (a: Seq[(GameTimeState, PlayerId)]) =>
-    Json.fromValues(a.map { case (time, id) => Json.fromValues(Seq(Json.fromString(time.toString), Json.fromInt(id.id))) })
+  implicit val fromPlayerIdStringMap = fromPlayerIdKeyMap[String]
+  implicit val fromPlayerIdBooleanMap = fromPlayerIdKeyMap[Boolean]
+  implicit val fromPlayerIdIntMap = fromPlayerIdKeyMap[Int]
+  implicit val fromPlayerIdFloatMap = fromPlayerIdKeyMap[Float]
+  implicit val fromPlayerIdIntTupleMap = fromPlayerIdKeyMap[(Int, Int)]
+  implicit val fromTeamIntMap = fromTeamKeyMap[Int]
+  implicit val fromTeamTimeMap = fromTeamKeyMap[GameTimeState]
 
   implicit def analysisEncoder: Encoder[AnalysisResult] = new Encoder[AnalysisResult] {
     override def apply(a: AnalysisResult): Json = Json.obj(
-      "heroes" -> stringMapEncoder(a.heroName),
-      "couriers" -> boolMapEncoder(a.couriers),
-      "ability_pt" -> tupleIntMapEncoder(a.abilityUsagesWithPT),
-      "resource_pt" -> tupleIntMapEncoder(a.resourceItemsUsagesWithPT),
-      "ptNotOnStrength" -> fromTimePlayerIdSeq(a.ptNotOnStrength),
-      "summon_gold" -> intMapEncoder(a.goldFedWithSummons),
-      "smoke_max_count_time" -> teamToInt(a.maxStockSmokesDuration),
-      "obs_max_count_time" -> teamToInt(a.maxStockObsDuration),
-      "smokes_used_on_vision" -> fromTimePlayerIdSeq(a.smokesUsedOnVision),
-      "obs_placed_on_vision" -> fromTimePlayerIdSeq(a.obsPlacedOnVision),
-      "unusedAbilities" -> timeIdStringSeq(a.unusedAbilities),
-      "unusedOnAllyAbilities" -> timeIdIdStringSeq(a.unusedOnAllyAbilities),
-      "unusedItems" -> timeIdStringSeq(a.unusedItems),
-      "purchases" -> fromPurchases(a.purchases),
-      "midasEfficiency" -> floatMapEncoder(a.midasEfficiency),
-      "wastedCreepwaves" -> fromWastedCreepwaves(a.wastedCreepwaves),
-      "badFights" -> Json.fromValues(a.badFights.map(fight => fromStringSeq(Seq(fight.outnumberedTeam.get.id.toString, fight.start.toString))))
+      "heroes" -> a.heroName.asJson,
+      "couriers" -> a.couriers.asJson,
+      "ability_pt" -> a.abilityUsagesWithPT.asJson,
+      "resource_pt" -> a.resourceItemsUsagesWithPT.asJson,
+      "ptNotOnStrength" -> a.ptNotOnStrength.asJson,
+      "summon_gold" -> a.goldFedWithSummons.asJson,
+      "smoke_max_count_time" -> a.maxStockSmokesDuration.asJson,
+      "obs_max_count_time" -> a.maxStockObsDuration.asJson,
+      "smokes_used_on_vision" -> a.smokesUsedOnVision.asJson,
+      "obs_placed_on_vision" -> a.obsPlacedOnVision.asJson,
+      "unusedAbilities" -> a.unusedAbilities.asJson,
+      "unusedOnAllyAbilities" -> a.unusedOnAllyAbilities.asJson,
+      "unusedItems" -> a.unusedItems.asJson,
+      "purchases" -> a.purchases.asJson,
+      "midasEfficiency" -> a.midasEfficiency.asJson,
+      "wastedCreepwaves" -> a.wastedCreepwaves.asJson,
+      "badFights" -> a.badFights.asJson
     )
   }
 

97eb6b1b2ea83a4a3f6e5fc4b1c9f3ac04233d0b
Roman Mendaliev <reacheight8@gmail.com>
2022-02-18 21:08:02 +0300

    Simplify json encoding even more

--- src/main/scala/wind/WindApp.scala
+++ src/main/scala/wind/WindApp.scala
@@ -17,11 +17,8 @@ import java.nio.file.{Files, Paths}
 import scala.concurrent.ExecutionContext.global
 
 object WindApp extends IOApp {
-  def fromPlayerIdKeyMap[T](implicit valueEncoder: Encoder[T]): Encoder[Map[PlayerId, T]] = (map: Map[PlayerId, T]) =>
-    map.map { case (id, value) => id.id -> value }.asJson
-
-  def fromTeamKeyMap[T](implicit valueEncoder: Encoder[T]): Encoder[Map[Team, T]] = (map: Map[Team, T]) =>
-    map.map { case (team, value) => team.id -> value }.asJson
+  implicit val playerIdToString = (id: PlayerId) => id.id.toString
+  implicit val teamToString = (team: Team) => team.id.toString
 
   implicit val fromPlayerId: Encoder[PlayerId] = (id: PlayerId) => id.id.asJson
   implicit val fromTeam: Encoder[Team] = (team: Team) => team.id.asJson
@@ -30,31 +27,27 @@ object WindApp extends IOApp {
   implicit val fromGameTimeState: Encoder[GameTimeState] = (time: GameTimeState) => time.toString.asJson
   implicit val fromFight: Encoder[Fight] = (fight: Fight) => (fight.outnumberedTeam.get, fight.start).asJson
 
-  implicit val fromPlayerIdStringMap = fromPlayerIdKeyMap[String]
-  implicit val fromPlayerIdBooleanMap = fromPlayerIdKeyMap[Boolean]
-  implicit val fromPlayerIdIntMap = fromPlayerIdKeyMap[Int]
-  implicit val fromPlayerIdFloatMap = fromPlayerIdKeyMap[Float]
-  implicit val fromPlayerIdIntTupleMap = fromPlayerIdKeyMap[(Int, Int)]
-  implicit val fromTeamIntMap = fromTeamKeyMap[Int]
-  implicit val fromTeamTimeMap = fromTeamKeyMap[GameTimeState]
+  implicit class MapExtensions[K, V](val map: Map[K, V]) {
+    def toStringKeyMap(implicit keyToString: K => String) = map.map { case (k, v) => keyToString(k) -> v }
+  }
 
-  implicit def analysisEncoder: Encoder[AnalysisResult] = new Encoder[AnalysisResult] {
+  implicit def analysisEncoder = new Encoder[AnalysisResult] {
     override def apply(a: AnalysisResult): Json = Json.obj(
-      "heroes" -> a.heroName.asJson,
-      "couriers" -> a.couriers.asJson,
-      "ability_pt" -> a.abilityUsagesWithPT.asJson,
-      "resource_pt" -> a.resourceItemsUsagesWithPT.asJson,
+      "heroes" -> a.heroName.toStringKeyMap.asJson,
+      "couriers" -> a.couriers.toStringKeyMap.asJson,
+      "ability_pt" -> a.abilityUsagesWithPT.toStringKeyMap.asJson,
+      "resource_pt" -> a.resourceItemsUsagesWithPT.toStringKeyMap.asJson,
       "ptNotOnStrength" -> a.ptNotOnStrength.asJson,
-      "summon_gold" -> a.goldFedWithSummons.asJson,
-      "smoke_max_count_time" -> a.maxStockSmokesDuration.asJson,
-      "obs_max_count_time" -> a.maxStockObsDuration.asJson,
+      "summon_gold" -> a.goldFedWithSummons.toStringKeyMap.asJson,
+      "smoke_max_count_time" -> a.maxStockSmokesDuration.toStringKeyMap.asJson,
+      "obs_max_count_time" -> a.maxStockObsDuration.toStringKeyMap.asJson,
       "smokes_used_on_vision" -> a.smokesUsedOnVision.asJson,
       "obs_placed_on_vision" -> a.obsPlacedOnVision.asJson,
       "unusedAbilities" -> a.unusedAbilities.asJson,
       "unusedOnAllyAbilities" -> a.unusedOnAllyAbilities.asJson,
       "unusedItems" -> a.unusedItems.asJson,
       "purchases" -> a.purchases.asJson,
-      "midasEfficiency" -> a.midasEfficiency.asJson,
+      "midasEfficiency" -> a.midasEfficiency.toStringKeyMap.asJson,
       "wastedCreepwaves" -> a.wastedCreepwaves.asJson,
       "badFights" -> a.badFights.asJson
     )

9d45c9008c35fabf20ca07c4eb9becf93fd722c7
Roman Mendaliev <reacheight8@gmail.com>
2022-03-27 20:20:37 +0300

    Modifier processor now stores information about shards and scepters

--- src/main/scala/wind/processors/ModifierProcessor.scala
+++ src/main/scala/wind/processors/ModifierProcessor.scala
@@ -3,18 +3,31 @@ package wind.processors
 import skadistats.clarity.model.{CombatLogEntry, Entity}
 import skadistats.clarity.processor.entities.OnEntityCreated
 import skadistats.clarity.processor.gameevents.OnCombatLogEntry
+import skadistats.clarity.processor.modifiers.OnModifierTableEntry
 import skadistats.clarity.processor.runner.Context
+import skadistats.clarity.processor.stringtables.{StringTables, UsesStringTable}
+import skadistats.clarity.wire.common.proto.DotaModifiers
+import skadistats.clarity.wire.common.proto.DotaModifiers.DOTA_MODIFIER_ENTRY_TYPE
 import skadistats.clarity.wire.common.proto.DotaUserMessages.DOTA_COMBATLOG_TYPES
+import wind.Util
 import wind.models.PlayerId
 
 import scala.collection.mutable
 
+@UsesStringTable("ModifierNames")
 class ModifierProcessor extends EntitiesProcessor {
   def smokedHeroes: Set[PlayerId] = _smoked.toSet
+  def scepter: Set[PlayerId] = _scepter.toSet
+  def shard: Set[PlayerId] = _shard.toSet
 
   private val _smoked = mutable.Set.empty[PlayerId]
+  private val _scepter = mutable.Set.empty[PlayerId]
+  private val _shard = mutable.Set.empty[PlayerId]
 
   private val SmokeModifierName = "modifier_smoke_of_deceit"
+  private val ShardModifierName = "modifier_item_aghanims_shard"
+  private val ScepterModifierName = "modifier_item_ultimate_scepter"
+
   private var combatLogHeroNameToPlayerId = Map.empty[String, Int]
 
   @OnEntityCreated(classPattern = "CWorld")
@@ -33,26 +46,31 @@ class ModifierProcessor extends EntitiesProcessor {
     }
   }
 
-    // will keep it there for future :clown_face:
-//  @OnModifierTableEntry
-//  def onModifierEntry(modifier: DotaModifiers.CDOTAModifierBuffTableEntry): Unit = {
-//    val gameRules = Entities.getByDtName("CDOTAGamerulesProxy")
-//    if (gameRules == null) return
-//
-//    val time = Util.getGameTimeState(gameRules)
-//    val table = ctx.getProcessor(classOf[StringTables]).forName("ModifierNames")
-//    val modifierName = table.getNameByIndex(modifier.getModifierClass)
-//
-//
-//    if (modifierName.contains("smoke") && modifier.getEntryType == DOTA_MODIFIER_ENTRY_TYPE.DOTA_MODIFIER_ENTRY_TYPE_ACTIVE) {
-//      val hero = Entities.getByHandle(modifier.getParent)
-//      _smoked.addOne(modifier.getSerialNum -> Util.getPlayerId(hero))
-//      println(s"${hero.getDtClass.getDtName} smoked with num ${modifier.getSerialNum} at $time")
-//    }
-//
-//    if (modifier.getEntryType == DOTA_MODIFIER_ENTRY_TYPE.DOTA_MODIFIER_ENTRY_TYPE_REMOVED && _smoked.contains(modifier.getSerialNum)) {
-//      _smoked.remove(modifier.getSerialNum)
-//      println(s"remove ${modifier.getSerialNum} at $time")
-//    }
-//  }
+  @OnModifierTableEntry
+  def onModifierEntry(ctx: Context, modifier: DotaModifiers.CDOTAModifierBuffTableEntry): Unit = {
+    val table = ctx.getProcessor(classOf[StringTables]).forName("ModifierNames")
+    val modifierName = table.getNameByIndex(modifier.getModifierClass)
+    val hero = Entities.getByHandle(modifier.getParent)
+    if (hero == null) return
+
+    if (modifierName == ScepterModifierName) {
+      val playerId = Util.getPlayerId(hero)
+
+      if (modifier.getEntryType == DOTA_MODIFIER_ENTRY_TYPE.DOTA_MODIFIER_ENTRY_TYPE_ACTIVE)
+        _scepter.addOne(playerId)
+
+      if (modifier.getEntryType == DOTA_MODIFIER_ENTRY_TYPE.DOTA_MODIFIER_ENTRY_TYPE_REMOVED)
+        _scepter.remove(playerId)
+    }
+
+    if (modifierName == ShardModifierName) {
+      val playerId = Util.getPlayerId(hero)
+
+      if (modifier.getEntryType == DOTA_MODIFIER_ENTRY_TYPE.DOTA_MODIFIER_ENTRY_TYPE_ACTIVE)
+        _shard.addOne(playerId)
+
+      if (modifier.getEntryType == DOTA_MODIFIER_ENTRY_TYPE.DOTA_MODIFIER_ENTRY_TYPE_REMOVED)
+        _shard.remove(playerId)
+    }
+  }
 }

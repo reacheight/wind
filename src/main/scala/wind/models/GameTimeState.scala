c292ca4c2aa253c1b556b4da6f0fc291c4f375a8
Roman Mendaliev <reacheight8@gmail.com>
2022-03-31 19:19:45 +0300

    Fix time toString bugs + implement addSeconds

--- src/main/scala/wind/models/GameTimeState.scala
+++ src/main/scala/wind/models/GameTimeState.scala
@@ -1,10 +1,17 @@
 package wind.models
 
 case class GameTimeState(preGameStarted: Boolean, gameStarted: Boolean, gameTime: Float) {
+  def addSeconds(seconds: Float): GameTimeState = {
+    val newSeconds = gameTime + seconds
+    val newGameStarted = newSeconds >= 0
+    GameTimeState(true, newGameStarted, newSeconds)
+  }
+
   override def toString: String = {
-    val minutes = gameTime.toInt.sign * gameTime.toInt.abs / 60
+    val minutes = gameTime.toInt.abs / 60
     val seconds = gameTime.toInt.abs % 60
     val secondsStr = if (seconds < 10) s"0$seconds" else seconds
-    if (preGameStarted) s"$minutes:$secondsStr" else "not started"
+    val sign = if (gameStarted) "" else "-"
+    if (preGameStarted) s"$sign$minutes:$secondsStr" else "not started"
   }
 }

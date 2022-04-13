28069e943c29a97a86fcb288192a4145fff85e19
Roman Mendaliev <reacheight8@gmail.com>
2022-04-13 20:35:49 +0300

    Add contact info into footer

--- ui/src/components/Footer.js
+++ ui/src/components/Footer.js
@@ -3,6 +3,8 @@ import styles from '../styles/Footer.module.css'
 const Footer = () =>
   <div className={styles.footer}>
     <span>Made by <a target="_blank" href="https://github.com/reacheight">@reacheight</a></span>
+    <br/>
+    <span>Contact me about anything at Discord рома#8350 or join <a target="_blank" href="https://discord.gg/GeGuH9dC">windota server</a></span>
   </div>
 
 export default Footer

6cc21c374aaea90f98ec7bd1d33fb8cb12b86a6f
Roman Mendaliev <reacheight8@gmail.com>
2022-04-15 13:54:20 +0300

    Get heroes Id

--- src/main/scala/wind/processors/HeroProcessor.scala
+++ src/main/scala/wind/processors/HeroProcessor.scala
@@ -4,16 +4,19 @@ package processors
 import skadistats.clarity.model.Entity
 import skadistats.clarity.processor.entities.OnEntityCreated
 import skadistats.clarity.wire.common.proto.Demo.CDemoFileInfo
+import wind.constants.Heroes
 
 class HeroProcessor(replayInfo: CDemoFileInfo) {
   private val game = replayInfo.getGameInfo.getDota
 
   def heroName: Map[Int, String] = heroNameBuilder.result
   def heroHandle: Map[Int, Int] = heroHandleBuilder.result
+  def heroId: Map[Int, Int] = heroIdBuilder.result
   val combatLogNameToPlayerId: Map[String, Int] = (0 to 9).map(id => game.getPlayerInfo(id).getHeroName -> id * 2).toMap
 
   private val heroNameBuilder = Map.newBuilder[Int, String]
   private val heroHandleBuilder = Map.newBuilder[Int, Int]
+  private val heroIdBuilder = Map.newBuilder[Int, Int] // PlayerId -> HeroId
 
   @OnEntityCreated(classPattern = "CDOTA_Unit_Hero_.*")
   def onHeroCreated(hero: Entity): Unit = {
@@ -25,5 +28,9 @@ class HeroProcessor(replayInfo: CDemoFileInfo) {
 
     heroNameBuilder += (playerId -> name)
     heroHandleBuilder += (playerId -> handle)
+
+    val cleName = combatLogNameToPlayerId.map(_.swap)(playerId)
+    val heroId = Heroes.getId(cleName)
+    heroIdBuilder += (playerId -> heroId)
   }
 }

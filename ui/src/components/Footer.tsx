2925c857c2e7b3c18a3baebc139d64ba9e95a903
Roman Mendaliev <reacheight8@gmail.com>
2022-05-15 22:32:11 +0300

    Fix discord server invite link

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () =>
   <div className={styles.footer}>
     <span>Made by <a target="_blank" href="https://github.com/reacheight">@reacheight</a></span>
     <br/>
-    <span>Contact me about anything at Discord рома#8350 or join <a target="_blank" href="https://discord.gg/GeGuH9dC">windota server</a></span>
+    <span>Contact me about anything at Discord рома#8350 or join <a target="_blank" href="https://discord.gg/5pjkNzMRjC">windota server</a></span>
   </div>
 
 export default Footer

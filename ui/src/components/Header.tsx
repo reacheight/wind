042bbb31d3f7a74d10f0b8bfbd9685f300a5da8a
Roman Mendaliev <reacheight8@gmail.com>
2022-06-17 18:45:50 +0300

    wind -> windota

--- ui/src/components/Header.tsx
+++ ui/src/components/Header.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Header.module.css'
 
 const Header = () =>
   <div className={styles.header}>
-    <span className={styles.title}>wind</span> <span className={styles.description}>Dota 2 replay analysis tool</span>
+    <span className={styles.title}>windota</span> <span className={styles.description}>Dota 2 replay analysis tool</span>
   </div>
 
 export default Header

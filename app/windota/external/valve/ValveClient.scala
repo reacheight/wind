0a88a71dd714d8be027b0ffe5f22867fa19be825
Roman Mendaliev <reacheight8@gmail.com>
2022-08-08 18:42:12 +0300

    Save downloaded replay file

--- app/windota/external/valve/ValveClient.scala
+++ app/windota/external/valve/ValveClient.scala
@@ -5,7 +5,8 @@ import sttp.client3.UriContext
 import sttp.client3.quick.{asFile, backend, quickRequest}
 import windota.models.ReplayLocation
 
-import java.nio.file.Path
+import java.io.{File, FileInputStream}
+import java.nio.file.{Files, Path}
 import scala.util.{Failure, Success, Try}
 
 object ValveClient {
@@ -20,9 +21,17 @@ object ValveClient {
       .send(backend)
 
     response.body match {
-      case Right(_) => Success()
+      case Right(file) =>
+        saveFile(file)
+        Success()
       case Left(string) => Failure(new Exception(string))
     }
+  }
+
+  private def saveFile(file: File) = {
+    file.createNewFile()
+    val inputStream = new FileInputStream(file)
+    Files.write(file.toPath, inputStream.readAllBytes())
   }
 
   private def getReplayUri(location: ReplayLocation) = uri"http://replay${location.cluster}.valve.net/570/${location.matchId}_${location.salt}.dem.bz2"

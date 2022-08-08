5e07356e2ea02889a0610ecad84a47749e422015
Roman Mendaliev <reacheight8@gmail.com>
2022-08-08 19:17:12 +0300

    Move icons images to conf to fix hosting

--- app/controllers/IconsController.scala
+++ app/controllers/IconsController.scala
@@ -9,7 +9,7 @@ import javax.inject.Inject
 class IconsController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
   def getIcon(heroId: Int) = Action {
     val heroTag = Heroes.getTag(heroId)
-    val imagePath = Paths.get(s"public/images/icons/$heroTag.png")
+    val imagePath = Paths.get(s"conf/icons/$heroTag.png")
     val image = Files.readAllBytes(imagePath)
     Ok(image).as("image/png")
   }

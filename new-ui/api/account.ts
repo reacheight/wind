fa913820df15d226ddaa7d066e68a73b6240e2bf
Roman Mendaliev <reacheight8@gmail.com>
2022-08-10 21:44:55 +0300

    Add logout menu

--- new-ui/api/account.ts
+++ new-ui/api/account.ts
@@ -1,4 +1,5 @@
 export default abstract class AccountRoutes {
   public static login = `${process.env.NEXT_PUBLIC_BACKEND_HOST}/login`
+  public static logout = `${process.env.NEXT_PUBLIC_BACKEND_HOST}/logout`
   public static user = `${process.env.NEXT_PUBLIC_BACKEND_HOST}/user`
 }
--- new-ui/components/Miniprofile.tsx
+++ new-ui/components/Miniprofile.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { getUserContext } from "./UserContextWrapper";
 import LoginButton from "./LoginButton";
+import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+import AccountRoutes from "../api/account";
 
 const Miniprofile = () => {
   let userContext = getUserContext()
@@ -11,7 +13,16 @@ const Miniprofile = () => {
   if (userContext.user === null)
     return <LoginButton />
 
-  return <Image src={userContext.user.avatarUrl} width={50} height={50}></Image>
+  return (
+    <Menu autoSelect={false}>
+      <MenuButton>
+        <Image src={userContext.user.avatarUrl} width={50} height={50}></Image>
+      </MenuButton>
+      <MenuList border={'none'} bg={"whiteAlpha.200"}>
+        <a href={AccountRoutes.logout}><MenuItem fontWeight={'600'}>Log Out</MenuItem></a>
+      </MenuList>
+    </Menu>
+  )
 }
 
 export default Miniprofile

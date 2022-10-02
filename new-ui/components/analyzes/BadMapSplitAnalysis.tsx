42c6dd73a78705e86b7fc7d12560896b09c8c085
Roman Mendaliev <reacheight8@gmail.com>
2022-10-02 20:32:22 +0300

    Less shitty bad split analysis view

--- new-ui/components/analyzes/BadMapSplitAnalysis.tsx
+++ new-ui/components/analyzes/BadMapSplitAnalysis.tsx
@@ -2,7 +2,7 @@ import { BadFight } from "../../models/BadFight";
 import styles from "../../styles/CommonAnalysis.module.css";
 import { useDisclosure } from "@chakra-ui/hooks";
 import { Collapse } from "@chakra-ui/transition";
-import { Box, HStack, Stack } from "@chakra-ui/layout";
+import { Stack } from "@chakra-ui/layout";
 import MiniIcon from "../MiniIcon";
 
 interface BadMapSplitProps {
@@ -16,7 +16,10 @@ const BadMapSplitAnalysis = ({ badFights }: BadMapSplitProps) => {
     let time = <span className={styles.time}>{fight.time}</span>
     let seenHeroesIcons = fight.seenHeroes.map(h => <MiniIcon heroId={h} />)
 
-    return <div className={styles.entry}>{time} <Box display={'flex'}>{seenHeroesIcons} showed up on enemy vision far from their teammates sdfs dfsd fsdf</Box></div>
+    return <div className={styles.entry}>
+      {time}
+      {seenHeroesIcons} showed up on enemy vision far from their teammates who got outnumbered in the fight
+    </div>
   })
 
   return (

ec9466b14a3ccd6537874e2c6a63eb6acff68dbe
Roman Mendaliev <reacheight8@gmail.com>
2022-10-04 21:18:17 +0300

    Add example image to landing

--- new-ui/components/Footer.tsx
+++ new-ui/components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <a className={styles.minilogo} href={"https://discord.gg"}><Image display={'block'} src="/discord.png" width={24} height={26} /></a>
+      <a href={"https://discord.gg/5pjkNzMRjC"}><Image display={'block'} src="/discord.png" width={24} height={26} /></a>
       <div>Dota 2 is a registered trademark of Valve Corporation.</div>
     </div>
   )
--- new-ui/components/Landing.tsx
+++ new-ui/components/Landing.tsx
@@ -1,5 +1,6 @@
 import styles from "../styles/Landing.module.css";
 import LoginButton from "./LoginButton";
+import Image from "next/image";
 
 const Landing = () =>
   <div>
@@ -9,6 +10,9 @@ const Landing = () =>
       </h1>
       <div className={styles.loginButton}>
         <LoginButton/>
+      </div>
+      <div className={styles.example}>
+        <Image src={"/example.png"} width={1601} height={925} />
       </div>
     </main>
   </div>

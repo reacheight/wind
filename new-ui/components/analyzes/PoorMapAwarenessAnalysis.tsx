f4d9f82a8cb2e85be758e9daae61c5ae06ee4d84
Roman Mendaliev <reacheight8@gmail.com>
2022-11-01 18:00:31 +0300

    Fix bad smoke fight front

--- new-ui/components/analyzes/PoorMapAwarenessAnalysis.tsx
+++ new-ui/components/analyzes/PoorMapAwarenessAnalysis.tsx
@@ -1,7 +1,7 @@
 import {BadSmokeFight} from "../../models/BadSmokeFight";
 import styles from "../../styles/CommonAnalysis.module.css";
-import { Stack } from "@chakra-ui/layout";
-import { Collapse, useDisclosure } from "@chakra-ui/react";
+import {Stack} from "@chakra-ui/layout";
+import {Collapse, useDisclosure} from "@chakra-ui/react";
 import {getOppositeTeam, Team} from "../../models/Team";
 
 interface PoorMapAwarenessProps {
@@ -12,14 +12,14 @@ const PoorMapAwarenessAnalysis = ({ badSmokeFights }: PoorMapAwarenessProps) => 
   const { isOpen, onToggle } = useDisclosure({ defaultIsOpen: true})
 
   const entries = badSmokeFights.map(fight => {
-    let smokeTime = <span className={styles.name}>{fight.smokeTime}</span>
-    let fightTime = <span className={styles.time}>{fight.fightTime}</span>
-    let smokedTeam = <span className={styles.name}>{Team[fight.smokedTeam]}</span>
-    let badFightTeam = <span className={styles.name}>{Team[getOppositeTeam(fight.smokedTeam)]}</span>
+    let smokeTime = <span className={styles.time}>{fight.smokeTime}</span>
+    let badFightTeam = getOppositeTeam(fight.smokedTeam)
+    let smokedTeamName = <span className={fight.smokedTeam == Team.Radiant ? styles.radiant : styles.dire}>{Team[fight.smokedTeam]}</span>
+    let badFightTeamName = <span className={badFightTeam == Team.Radiant ? styles.radiant : styles.dire}>{Team[badFightTeam]}</span>
 
     return <div className={styles.entry}>
-      {fightTime}
-      {badFightTeam} saw {smokedTeam} use smoke at {smokeTime}, but didn't react and lost fight anyway
+      {smokeTime}
+      <span>{badFightTeamName} saw {smokedTeamName} use smoke, but didn't react and lost fight anyway</span>
     </div>
   })
 

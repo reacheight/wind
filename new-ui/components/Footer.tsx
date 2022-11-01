9153ec2e1716efc5fb1c7f5101b1f00bab296586
Roman Mendaliev <reacheight8@gmail.com>
2022-11-01 19:19:31 +0300

    Use discord icon from react icons

--- new-ui/components/Footer.tsx
+++ new-ui/components/Footer.tsx
@@ -1,10 +1,12 @@
 import styles from "../styles/Footer.module.css"
-import Image from "next/image";
+import {FaDiscord, SiDiscord} from "react-icons/all";
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
-      <a href={"https://discord.gg/5pjkNzMRjC"}><Image display={'block'} src="/discord.png" width={24} height={26} /></a>
+      <div className={styles.icons}>
+        <a href={"https://discord.gg/5pjkNzMRjC"}><SiDiscord size={'24px'} /></a>
+      </div>
       <div>Dota 2 is a registered trademark of Valve Corporation.</div>
     </div>
   )

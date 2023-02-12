a65ebeae8512541ec91430fe75a63f5acb120e33
Roman Mendaliev <reacheight8@gmail.com>
2023-02-12 13:39:04 +0300

    Show unused on dying allies abilities

--- new-ui/components/categories/Hud/UsageInfo.tsx
+++ new-ui/components/categories/Hud/UsageInfo.tsx
@@ -3,6 +3,7 @@ import { UnusedAbility } from "../../../models/UnusedAbility";
 import { OverlappedStun } from "../../../models/OverlappedStun";
 
 import styles from "./Hud.module.css";
+import MiniIcon from "../../MiniIcon";
 
 interface UsageInfoProps {
   selectedAbilityId: number
@@ -20,8 +21,9 @@ const UsageInfo = ({ selectedAbilityId, abilities, unusedAbilities, overlappedSt
 
   const selectedAbility = abilities.find(a => a.id === selectedAbilityId)
   const unusedSelectedAbility = unusedAbilities.filter(unusedAbility => unusedAbility.ability === selectedAbilityId)
+
   const unusedSelectedAbilityOnSelf = unusedSelectedAbility.filter(ua => ua.user === ua.target)
-  const unusedSelectedAbilityOnSelfTimes = unusedSelectedAbilityOnSelf.length === 0 ? [] : unusedSelectedAbilityOnSelf.map(ua =>
+  const unusedSelectedAbilityOnSelfTimes = unusedSelectedAbilityOnSelf.length === 0 ? null : unusedSelectedAbilityOnSelf.map(ua =>
     <span className={styles.time}>{ua.time}</span>
   ).reduce((prev, curr) => [prev, ', ', curr])
 
@@ -29,11 +31,23 @@ const UsageInfo = ({ selectedAbilityId, abilities, unusedAbilities, overlappedSt
     {unusedSelectedAbilityOnSelf.length} times <span className={styles.insight}>not used</span> before <span className={styles.insight}>death</span> at {unusedSelectedAbilityOnSelfTimes}
   </span>
 
+  const unusedAbilityOnAllies = unusedSelectedAbility.filter(ua => ua.user !== ua.target)
+  const unusedAbilityOnAlliesTimes = unusedAbilityOnAllies.length === 0 ? null : unusedAbilityOnAllies.map(ua => {
+    let time = <span className={styles.time}>{ua.time}</span>
+    let targetIcon = <MiniIcon heroId={ua.target} />
+    return <span>on {targetIcon} at {time}</span>
+  }).reduce((prev, curr) => [prev, ', ', curr])
+
+  const unusedAbilityOnAlliesSpan = <span>
+    {unusedAbilityOnAllies.length} times <span className={styles.insight}>not used</span> on a <span className={styles.insight}>dying ally</span> — {unusedAbilityOnAlliesTimes}
+  </span>
+
   return (
-    <div className={styles.mistakes}>
-      <span className={styles.mistakesTitle}>{selectedAbility.displayName}</span>
-      <div className={styles.mistakesEntries}>
-        {unusedSelectedAbilityOnSelf.length > 0 && <>{unusedSelectedAbilitySpan}</>}
+    <div className={styles.usages}>
+      <span className={styles.usagesTitle}>{selectedAbility.displayName}</span>
+      <div className={styles.usagesEntries}>
+        {unusedSelectedAbilityOnSelf.length > 0 && <div>{unusedSelectedAbilitySpan}</div>}
+        {unusedAbilityOnAllies.length > 0 && <div>{unusedAbilityOnAlliesSpan}</div>}
       </div>
     </div>
   )

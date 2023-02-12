add39e6a69a7888842f8a6a1ea557e633e29328d
Roman Mendaliev <reacheight8@gmail.com>
2023-02-12 13:50:14 +0300

    Show overlapped stuns

--- new-ui/components/categories/Hud/UsageInfo.tsx
+++ new-ui/components/categories/Hud/UsageInfo.tsx
@@ -21,6 +21,7 @@ const UsageInfo = ({ selectedAbilityId, abilities, unusedAbilities, overlappedSt
 
   const selectedAbility = abilities.find(a => a.id === selectedAbilityId)
   const unusedSelectedAbility = unusedAbilities.filter(unusedAbility => unusedAbility.ability === selectedAbilityId)
+  const overlappedAbilityStun = overlappedStuns.filter(stun => stun.isAbility && stun.sourceId == selectedAbilityId)
 
   const unusedSelectedAbilityOnSelf = unusedSelectedAbility.filter(ua => ua.user === ua.target)
   const unusedSelectedAbilityOnSelfTimes = unusedSelectedAbilityOnSelf.length === 0 ? null : unusedSelectedAbilityOnSelf.map(ua =>
@@ -42,12 +43,23 @@ const UsageInfo = ({ selectedAbilityId, abilities, unusedAbilities, overlappedSt
     {unusedAbilityOnAllies.length} times <span className={styles.insight}>not used</span> on a <span className={styles.insight}>dying ally</span> — {unusedAbilityOnAlliesTimes}
   </span>
 
+  const overlappedStunTimes = overlappedAbilityStun.length === 0 ? null : overlappedAbilityStun.map(stun => {
+    let time = <span className={styles.time}>{stun.time}</span>
+    let targetIcon = <MiniIcon heroId={stun.target} />
+    return <span>{targetIcon} at {time} ({stun.overlappedTime.toFixed(1)} seconds <span className={styles.insight}>overlap</span>)</span>
+  }).reduce((prev, curr) => [prev, ', ', curr])
+
+  const overlappedStunSpan = <span>
+    {overlappedAbilityStun.length} times <span className={styles.insight}>stunned</span> an <span className={styles.insight}>already disabled</span> hero — {overlappedStunTimes}
+  </span>
+
   return (
     <div className={styles.usages}>
       <span className={styles.usagesTitle}>{selectedAbility.displayName}</span>
       <div className={styles.usagesEntries}>
         {unusedSelectedAbilityOnSelf.length > 0 && <div>{unusedSelectedAbilitySpan}</div>}
         {unusedAbilityOnAllies.length > 0 && <div>{unusedAbilityOnAlliesSpan}</div>}
+        {overlappedAbilityStun.length >0 && <div>{overlappedStunSpan}</div>}
       </div>
     </div>
   )

2844a6d692031de66fe63d70237b847727845f40
Roman Mendaliev <reacheight8@gmail.com>
2023-02-27 22:53:32 +0300

    Make laning images even height

--- new-ui/components/categories/Laning/Courier.tsx
+++ new-ui/components/categories/Laning/Courier.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const Courier = () => (
   <div className={styles.container}>
-    <div><Image src={"/courier.webp"} width={140} height={140} /></div>
+    <div className={styles.courier}><Image src={"/courier.webp"} width={120} height={120} /></div>
     <span className={styles.text}>
       You didn't place your <span className={styles.insight}>courier out of the fountain</span> at&nbsp;the start of the game
     </span>

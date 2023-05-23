42d438e16abc977230b3f57500f433074b83c397
Roman Mendaliev <reacheight8@gmail.com>
2023-05-23 21:42:04 +0300

    Add sides image and winner tag

--- new-ui/components/Matchup.tsx
+++ new-ui/components/Matchup.tsx
@@ -2,6 +2,7 @@ import Match from "../models/Match";
 import styles from '../styles/Matchup.module.css'
 import HeroMatchPreview from "./HeroMatchPreview";
 import { HStack} from "@chakra-ui/layout";
+import Image from "next/image";
 
 interface MatchupProps {
   match: Match
@@ -17,11 +18,30 @@ const Matchup = ({ match }: MatchupProps) => {
   let radiantTeam = <HStack spacing={4}>{radiantHeroes}</HStack>
   let direTeam = <HStack spacing={4}>{direHeroes}</HStack>
 
+  const radiantWinner = <span className={styles.radiantWinner}>Winner</span>
+  const direWinner = <span className={styles.direWinner}>Winner</span>
+
   return (
-    <div className={styles.main}>
-      {radiantTeam}
-      <span className={styles.vs}>vs</span>
-      {direTeam}
+    <div>
+      <div className={styles.sides}>
+        <div className={styles.side}>
+          <div className={styles.radiant}>
+            <Image src={"/radiant.png"} width={60} height={60} />
+          </div>
+          {match.didRadiantWin && radiantWinner}
+        </div>
+        <div className={styles.side}>
+          {!match.didRadiantWin && direWinner}
+          <div className={styles.dire}>
+            <Image src={"/dire.png"} width={60} height={60} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.matchup}>
+        {radiantTeam}
+        <span className={styles.vs}>vs</span>
+        {direTeam}
+      </div>
     </div>
   )
 }

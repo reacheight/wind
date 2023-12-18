788092ff5aaf92270cd93f9ec4d8a20c09a5711e
Roman Mendaliev <reacheight8@gmail.com>
2023-12-18 20:41:12 +0300

    Add optional width and height parameters to mini icon

--- new-ui/components/MiniIcon.tsx
+++ new-ui/components/MiniIcon.tsx
@@ -4,9 +4,11 @@ import styles from "../styles/MiniIcon.module.css"
 
 interface MiniIconProps {
   heroId: number
+  width?: number
+  height?: number
 }
 
-const MiniIcon = ({ heroId }: MiniIconProps) =>
-  <span className={styles.icon}><Image src={Routes.Images.getIcon(heroId)} width={30} height={30} /></span>
+const MiniIcon = ({ heroId, width, height }: MiniIconProps) =>
+  <span className={styles.icon}><Image src={Routes.Images.getIcon(heroId)} width={width ?? 30} height={height ?? 30} /></span>
 
 export default MiniIcon

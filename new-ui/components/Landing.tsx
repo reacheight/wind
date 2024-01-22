fda51533e357fc38259043e2d872df699cfa5754
Roman Mendaliev <reacheight8@gmail.com>
2024-01-22 21:25:27 +0300

    Small changes in landing

--- new-ui/components/Landing.tsx
+++ new-ui/components/Landing.tsx
@@ -3,21 +3,19 @@ import LoginButton from "./LoginButton";
 import Image from "next/image";
 
 const Landing = () =>
-  <div>
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        Analyze your Dota 2 matches
-      </h1>
-      <div className={styles.description}>
-        Get insights about what could be done better in your games with our <span className={styles.beta}>[super early β]</span> replay analysis
-      </div>
-      <div className={styles.loginButton}>
-        <LoginButton/>
-      </div>
-      <div className={styles.example}>
-        <Image src={"/example.png"} width={1500} height={935} />
-      </div>
-    </main>
-  </div>
+  <main className={styles.main}>
+    <h1 className={styles.title}>
+      Analyze your Dota 2 matches
+    </h1>
+    <div className={styles.description}>
+      Get insights about what could be done better in your games with our <span className={styles.beta}>[super early β]</span> replay analysis assistant
+    </div>
+    <div className={styles.loginButton}>
+      <LoginButton/>
+    </div>
+    <div className={styles.example}>
+      <Image src={"/example.png"} width={1500} height={935} />
+    </div>
+  </main>
 
 export default Landing
